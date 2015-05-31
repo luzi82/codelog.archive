d40ab46121695dea1055234bd2fb66ede8578d91
Luzi82 <luzi82@gmail.com>
2015-05-31 12:11:11 +0800

    rename file

--- xz/test.sh
+++ xz/test.sh
@@ -7,8 +7,8 @@ cp -R res tmp
 
 cd tmp
 
-xz -z --keep data.jpg
+xz -z --keep data0.jpg
 
-xz -d data.jpg.xz -c > data.jpg.xz.-
+xz -d data0.jpg.xz -c > data0.jpg.xz.-
 
-diff data.jpg.xz.- data.jpg
+diff data0.jpg.xz.- data0.jpg
